9a0b219ff86bbb7107e474f1c82e06bd3eea63c4
oakes <zsoakes@gmail.com>
2014-02-18 16:00:59 -0500

    Use Nightcode as a library

--- desktop/project.clj
+++ desktop/project.clj
@@ -5,14 +5,8 @@
                  [com.badlogicgames.gdx/gdx-backend-lwjgl "0.9.9"]
                  [com.badlogicgames.gdx/gdx-platform "0.9.9"
                   :classifier "natives-desktop"]
-                 [com.github.insubstantial/substance "7.2.1"]
-                 [com.fifesoft/autocomplete "2.5.0"]
-                 [com.fifesoft/rsyntaxtextarea "2.5.0"]
-                 [compliment "0.0.3"]
+                 [nightcode "0.3.0-SNAPSHOT"]
                  [org.clojure/clojure "1.5.1"]
-                 [org.flatland/ordered "1.5.2"]
-                 [org.lpetit/paredit.clj "0.19.3"]
-                 [net.java.balloontip/balloontip "1.2.4.1"]
                  [play-clj "0.2.0"]
                  [seesaw "1.4.4"]]
   :repositories [["sonatype"
--- desktop/src/clojure/nightmod/core/desktop_launcher.clj
+++ desktop/src/clojure/nightmod/core/desktop_launcher.clj
@@ -1,26 +1,33 @@
 (ns nightmod.core.desktop-launcher
   (:require [clojure.java.io :as io]
             [nightmod.core :refer :all]
-            [nightmod.core.editors :as editors]
-            [nightmod.core.dialogs :as dialogs]
-            [nightmod.core.shortcuts :as shortcuts]
-            [nightmod.core.ui :as ui]
-            [nightmod.core.utils :as utils]
-            [seesaw.core :as s])
+            [nightcode.core :as core]
+            [nightcode.editors :as editors]
+            [nightcode.dialogs :as dialogs]
+            [nightcode.shortcuts :as shortcuts]
+            [nightcode.ui :as ui]
+            [nightcode.utils :as utils]
+            [seesaw.core :as s]
+            [seesaw.util :as s-util])
   (:import [java.awt BorderLayout Canvas Dimension Window]
            [java.awt.event ComponentAdapter WindowAdapter]
            [javax.swing JLayeredPane]
            [com.badlogic.gdx.backends.lwjgl LwjglApplication]
-           [org.fife.ui.rsyntaxtextarea FileLocation SyntaxConstants
-            TextEditorPane Theme]
            [org.lwjgl.input Keyboard]
            [org.pushingpixels.substance.api SubstanceLookAndFeel]
            [org.pushingpixels.substance.api.skin GraphiteSkin])
   (:gen-class))
 
+; allow s/select to work with Canvas
+(extend-protocol s-util/Children
+  java.awt.Component (children [this] nil))
+
 (def ^:const window-width 1200)
 (def ^:const window-height 768)
 (def ^:const editor-width 700)
+
+(def ^:dynamic editor-controls [:save :undo :redo :font-dec :font-inc
+                                :doc :paredit :paredit-help])
 
 (defn create-layered-pane
   "Returns the pane with the editors."
@@ -45,26 +52,19 @@
 (defn confirm-exit-app!
   "Displays a dialog confirming whether the program should shut down."
   []
-  (if (dialogs/show-shut-down-dialog!)
+  (if (dialogs/show-shut-down-dialog! [])
     (System/exit 0)
     true))
-
-(defn enable-full-screen!
-  "Enables full screen mode on OS X."
-  [window]
-  (try
-    (some-> (Class/forName "com.apple.eawt.FullScreenUtilities")
-            (.getMethod "setWindowCanFullScreen"
-                        (into-array Class [Window Boolean/TYPE]))
-            (.invoke nil (object-array [window true])))
-    (catch Exception _)))
 
 (defn create-window
   "Creates the main window."
   []
   (doto (s/frame :title (str (utils/get-string :app_name)
                              " "
-                             (utils/get-version))
+                             (if-let [p (utils/get-project
+                                          "nightmod.core.desktop_launcher")]
+                               (nth p 2)
+                               "beta"))
                  :width window-width
                  :height window-height
                  :on-close :nothing)
@@ -73,7 +73,7 @@
     (-> .getGlassPane (doto
                         (.setLayout (BorderLayout.))
                         (.add (create-layered-pane) BorderLayout/EAST)
-                        (.setVisible false)))
+                        (.setVisible true)))
     ; listen for keys while modifier is down
     (shortcuts/listen-for-shortcuts!
       (fn [key-code]
@@ -89,7 +89,7 @@
           ; else
           false)))
     ; set various window properties
-    enable-full-screen!
+    core/enable-full-screen!
     (.addWindowListener (proxy [WindowAdapter] []
                           (windowClosing [e]
                             (confirm-exit-app!))))))
@@ -100,5 +100,7 @@
   (s/native!)
   (SubstanceLookAndFeel/setSkin (GraphiteSkin.))
   (s/invoke-later
-    (s/show! (reset! ui/ui-root (create-window))))
+    (s/show! (reset! ui/ui-root (create-window)))
+    (binding [editors/editor-controls editor-controls]
+      (comment editors/show-editor! "")))
   (Keyboard/enableRepeatEvents true))
