8075b3983467a21d6cd62807d42941743e0228b0
oakes <zsoakes@gmail.com>
2014-02-28 16:44:10 -0500

    Switch back nightcode version

--- desktop/project.clj
+++ desktop/project.clj
@@ -5,7 +5,7 @@
                  [com.badlogicgames.gdx/gdx-backend-lwjgl "0.9.9"]
                  [com.badlogicgames.gdx/gdx-platform "0.9.9"
                   :classifier "natives-desktop"]
-                 [nightcode "0.3.1-SNAPSHOT"
+                 [nightcode "0.3.0"
                   :exclusions [leiningen
                                lein-ancient
                                lein-cljsbuild
