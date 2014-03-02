4528faa1c60d29d5c5fb1949d5edb452436b1123
oakes <zsoakes@gmail.com>
2014-03-02 15:04:44 -0500

    Make buttons identifiable

--- desktop/src-common/nightmod/core.clj
+++ desktop/src-common/nightmod/core.clj
@@ -7,14 +7,17 @@
   (fn [screen entities]
     (update! screen :renderer (stage) :camera (orthographic))
     (let [ui-skin (skin "uiskin.json")
-          saved-games (map #(text-button % ui-skin) [])]
-      (-> [(label "New Game:" ui-skin)
-           (text-button "Arcade" ui-skin)
-           (text-button "Platformer" ui-skin)
-           (text-button "Orthogonal RPG" ui-skin)
-           (text-button "Isometric RPG" ui-skin)
-           (text-button "Barebones 2D" ui-skin)
-           (text-button "Barebones 3D" ui-skin)]
+          create-button (fn [[k v]]
+                          (text-button v ui-skin :set-name k))
+          templates [["arcade" "Arcade"]
+                     ["platformer" "Platformer"]
+                     ["orthogonal-rpg" "Orthogonal RPG"]
+                     ["isometric-rpg" "Isometric RPG"]
+                     ["barebones-2d" "Barebones 2D"]
+                     ["barebones-3d" "Barebones 3D"]]
+          new-games (map create-button templates)
+          saved-games (map create-button [])]
+      (-> (cons (label "New Game:" ui-skin) new-games)
           (concat (when (seq saved-games)
                     (cons (label "Load Game:" ui-skin) saved-games)))
           vertical
