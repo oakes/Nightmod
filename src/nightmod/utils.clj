0f2916bb055f485766a76a2f50afa92e1011571c
oakes <zsoakes@gmail.com>
2014-03-19 17:10:59 -0400

    Simplify location of data dir

--- src/nightmod/utils.clj
+++ src/nightmod/utils.clj
@@ -10,18 +10,7 @@
 
 (defn get-data-dir
   []
-  (let [home-dir (System/getProperty "user.home")
-        app-name "Nightmod"
-        app-name-lower (clojure.string/lower-case app-name)
-        osx-dir (io/file home-dir "Library" "Application Support" app-name)
-        win-dir (io/file home-dir "AppData" "Roaming" app-name)]
-    (.getCanonicalPath
-      (cond
-        (.exists (.getParentFile osx-dir)) osx-dir
-        (.exists (.getParentFile win-dir)) win-dir
-        :else (if-let [config-dir (System/getenv "XDG_CONFIG_HOME")]
-                (io/file config-dir app-name-lower)
-                (io/file home-dir ".config" app-name-lower))))))
+  (.getCanonicalPath (io/file (System/getProperty "user.home") "nightmod")))
 
 (defn format-date
   [unix-time]
