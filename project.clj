a135169c22b3ef4a0cd65ffba271b916df27482b
oakes <zsoakes@gmail.com>
2014-04-20 23:01:28 -0400

    Update to play-clj 0.3.0 and LibGDX 1.0

--- project.clj
+++ project.clj
@@ -2,15 +2,15 @@
   :description "FIXME: write description"
   
   :dependencies [[clojail "1.0.6"]
-                 [com.badlogicgames.gdx/gdx "1.0-SNAPSHOT"]
-                 [com.badlogicgames.gdx/gdx-backend-lwjgl "1.0-SNAPSHOT"]
-                 [com.badlogicgames.gdx/gdx-box2d "1.0-SNAPSHOT"]
-                 [com.badlogicgames.gdx/gdx-box2d-platform "1.0-SNAPSHOT"
+                 [com.badlogicgames.gdx/gdx "1.0.0"]
+                 [com.badlogicgames.gdx/gdx-backend-lwjgl "1.0.0"]
+                 [com.badlogicgames.gdx/gdx-box2d "1.0.0"]
+                 [com.badlogicgames.gdx/gdx-box2d-platform "1.0.0"
                   :classifier "natives-desktop"]
-                 [com.badlogicgames.gdx/gdx-bullet "1.0-SNAPSHOT"]
-                 [com.badlogicgames.gdx/gdx-bullet-platform "1.0-SNAPSHOT"
+                 [com.badlogicgames.gdx/gdx-bullet "1.0.0"]
+                 [com.badlogicgames.gdx/gdx-bullet-platform "1.0.0"
                   :classifier "natives-desktop"]
-                 [com.badlogicgames.gdx/gdx-platform "1.0-SNAPSHOT"
+                 [com.badlogicgames.gdx/gdx-platform "1.0.0"
                   :classifier "natives-desktop"]
                  [com.cemerick/pomegranate "0.3.0"]
                  [nightcode "0.3.3-SNAPSHOT"
@@ -22,10 +22,8 @@
                                play-clj/lein-template]]
                  [org.clojure/clojure "1.6.0"]
                  [org.eclipse.jgit "3.2.0.201312181205-r"]
-                 [play-clj "0.3.0-SNAPSHOT"]
+                 [play-clj "0.3.0"]
                  [seesaw "1.4.4"]]
-  :repositories [["sonatype"
-                  "https://oss.sonatype.org/content/repositories/snapshots/"]]
   
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
   :aot [nightmod.core]
