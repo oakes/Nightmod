e97989439b2d52301316e4f2466197c801e00d33
oakes <zsoakes@gmail.com>
2014-05-11 14:47:30 -0400

    Add restart-game! function

--- src/clojure/nightmod/public.clj
+++ src/clojure/nightmod/public.clj
@@ -1,15 +1,19 @@
 (ns nightmod.public
   (:require [clojail.core :as jail]
             [clojure.java.io :as io]
-            [nightmod.screens :as s]
+            [nightmod.screens :as screens]
             [nightmod.utils :as u]
             [play-clj.core :refer :all]))
 
 (defn set-game-screen!
-  [& screens]
-  (->> (conj (vec screens) s/overlay-screen)
-       (apply set-screen! s/nightmod)
+  [& game-screens]
+  (->> (conj (vec game-screens) screens/overlay-screen)
+       (apply set-screen! screens/nightmod)
        on-gl))
+
+(defn restart-game!
+  []
+  (on-gl (screens/restart!)))
 
 (defmacro load-game-file
   [n]
