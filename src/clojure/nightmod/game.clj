07da8cb135b2de1faf23f67f57c38a8c60e9b89c
oakes <zsoakes@gmail.com>
2014-05-19 04:15:38 -0400

    Add nightmod.game

---
+++ src/clojure/nightmod/game.clj
@@ -0,0 +1,33 @@
+(ns nightmod.game
+  (:require [clojail.core :as jail]
+            [clojure.java.io :as io]
+            [nightmod.screens :as screens]
+            [nightmod.utils :as u]
+            [play-clj.core :refer :all]))
+
+(defn set-game-screen!
+  "Displays one or more screens that you defined with `defscreen`. They will be
+run the order they are passed in. Note that only the first screen should call
+`(clear!)` in its :on-render function; if the others do, they will clear
+whatever was drawn by the preceding screens.
+
+    (set-game-screen! main-screen text-screen)"
+  [& game-screens]
+  (->> (conj (vec game-screens) screens/overlay-screen)
+       (apply set-screen! screens/nightmod)
+       on-gl))
+
+(defn restart-game!
+  "Causes the core.clj file to be run again."
+  []
+  (on-gl (screens/restart!)))
+
+(defmacro load-game-file
+  "Loads a file into the namespace.
+
+    (load-game-file \"utils.clj\")"
+  [n]
+  (some->> (io/file @u/project-dir n)
+           slurp
+           (format "(do %s\n)")
+           jail/safe-read))
