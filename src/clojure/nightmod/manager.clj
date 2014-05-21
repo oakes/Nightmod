f98c9d77ce62184b46d3547562e4705632057921
oakes <zsoakes@gmail.com>
2014-05-20 23:20:35 -0400

    Bring back timer stopping code

--- src/clojure/nightmod/manager.clj
+++ src/clojure/nightmod/manager.clj
@@ -12,6 +12,22 @@
                    (FileHandle. (io/file @u/project-dir file-name))))))
 (set-asset-manager! manager)
 
+; keep a reference to all timers to we can stop them later
+(def timers (atom []))
+(let [create-and-add-timer! (deref #'play-clj.core/create-and-add-timer!)]
+  (intern 'play-clj.core
+          'create-and-add-timer!
+          (fn [screen id]
+            (let [t (create-and-add-timer! screen id)]
+              (swap! timers conj t)
+              t))))
+
+(defn stop-timers!
+  []
+  (doseq [t @timers]
+    (.stop t))
+  (reset! timers []))
+
 (defn clear-ns!
   [nspace]
   (doall (map #(ns-unmap nspace %) (keys (ns-interns nspace)))))
@@ -19,4 +35,5 @@
 (defn clean!
   []
   (clear-ns! u/game-ns)
+  (stop-timers!)
   (on-gl (asset-manager! manager :clear)))
