56906de38fde65c03bb1dc814d147982974dda12
oakes <zsoakes@gmail.com>
2014-05-23 19:03:11 -0400

    Use KeyEventDispatcher instead of KeyListener

--- src/clojure/nightmod/input.clj
+++ src/clojure/nightmod/input.clj
@@ -1,6 +1,10 @@
 (ns nightmod.input
-  (:require [play-clj.core :as play-clj])
-  (:import [java.awt.event ComponentAdapter KeyListener]
+  (:require [nightcode.shortcuts :as shortcuts]
+            [nightcode.ui :as ui]
+            [nightmod.utils :as u]
+            [play-clj.core :as play-clj])
+  (:import [java.awt KeyboardFocusManager KeyEventDispatcher]
+           [java.awt.event ComponentAdapter KeyEvent]
            [com.badlogic.gdx.backends.lwjgl LwjglInput]
            [nightmod KeyCodeConverter]
            [org.lwjgl.input Keyboard]
@@ -27,28 +31,51 @@
         impl (proxy [InputImplementation] []
                (pollKeyboard [bb])
                (readKeyboard [bb]))]
-    (.addKeyListener
-      window
-      (reify KeyListener
-        (keyReleased [this e]
-          (.set impl-field nil impl)
-          (.put key-buf (awt->lwjgl (.getKeyCode e)) (byte 0))
-          (-> game
-              .getInput
-              .getInputProcessor
-              (.keyUp (awt->libgdx (.getKeyCode e)))
-              play-clj/on-gl))
-        (keyTyped [this e]
-          (-> game
-              .getInput
-              .getInputProcessor
-              (.keyTyped (.getKeyChar e))
-              play-clj/on-gl))
-        (keyPressed [this e]
-          (.set impl-field nil impl)
-          (.put key-buf (awt->lwjgl (.getKeyCode e)) (byte 8))
-          (-> game
-              .getInput
-              .getInputProcessor
-              (.keyDown (awt->libgdx (.getKeyCode e)))
-              play-clj/on-gl))))))
+    (.addKeyEventDispatcher
+      (KeyboardFocusManager/getCurrentKeyboardFocusManager)
+      (proxy [KeyEventDispatcher] []
+        (dispatchKeyEvent [^KeyEvent e]
+          (cond
+            ; a key was released in a game
+            (and @ui/tree-selection
+                 (shortcuts/focused-window? window)
+                 (u/editor-hidden?)
+                 (= (.getID e) KeyEvent/KEY_RELEASED))
+            (do
+              (.set impl-field nil impl)
+              (.put key-buf (awt->lwjgl (.getKeyCode e)) (byte 0))
+              (-> game
+                  .getInput
+                  .getInputProcessor
+                  (.keyUp (awt->libgdx (.getKeyCode e)))
+                  play-clj/on-gl)
+              true)
+            ; a key was typed in a game
+            (and @ui/tree-selection
+                 (shortcuts/focused-window? window)
+                 (u/editor-hidden?)
+                 (= (.getID e) KeyEvent/KEY_TYPED))
+            (do
+              (-> game
+                  .getInput
+                  .getInputProcessor
+                  (.keyTyped (.getKeyChar e))
+                  play-clj/on-gl)
+              true)
+            ; a key was pressed in a game
+            (and @ui/tree-selection
+                 (shortcuts/focused-window? window)
+                 (u/editor-hidden?)
+                 (= (.getID e) KeyEvent/KEY_PRESSED))
+            (do
+              (.set impl-field nil impl)
+              (.put key-buf (awt->lwjgl (.getKeyCode e)) (byte 8))
+              (-> game
+                  .getInput
+                  .getInputProcessor
+                  (.keyDown (awt->libgdx (.getKeyCode e)))
+                  play-clj/on-gl)
+              true)
+            ; don't handle
+            :else
+            false))))))
