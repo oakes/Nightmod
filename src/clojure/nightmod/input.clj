4e322d4bc4d09642ed114bf5df4f2f5d9e6927f5
Zach Oakes <zsoakes@gmail.com>
2014-05-26 05:48:47 -0400

    Completely clear the key buffer when the command key is lifted

--- src/clojure/nightmod/input.clj
+++ src/clojure/nightmod/input.clj
@@ -2,7 +2,7 @@
   (:require [nightcode.shortcuts :as shortcuts]
             [play-clj.core :as play-clj])
   (:import [com.badlogic.gdx.backends.lwjgl LwjglInput]
-           [java.awt.event KeyListener]
+           [java.awt.event KeyEvent KeyListener]
            [nightmod KeyCodeConverter]
            [org.lwjgl.input Keyboard]
            [org.lwjgl.opengl InputImplementation]))
@@ -32,23 +32,32 @@
       window
       (reify KeyListener
         (keyReleased [this e]
+          ; clear the LWJGL key buffer
+          ; when the OS X command key is lifted, we must clear it
+          ; completely because it seems to swallow keyReleased events
           (.set impl-field nil impl)
-          (.put key-buf (awt->lwjgl (.getKeyCode e)) (byte 0))
+          (if (= (.getKeyCode e) KeyEvent/VK_META)
+            (doseq [i (range (.capacity key-buf))]
+              (.put key-buf i (byte 0)))
+            (.put key-buf (awt->lwjgl (.getKeyCode e)) (byte 0)))
+          ; pass the event to LibGDX
           (-> game
               .getInput
               .getInputProcessor
               (.keyUp (awt->libgdx (.getKeyCode e)))
               play-clj/on-gl))
         (keyTyped [this e]
+          ; pass the event to LibGDX
           (-> game
               .getInput
               .getInputProcessor
               (.keyTyped (.getKeyChar e))
               play-clj/on-gl))
         (keyPressed [this e]
+          ; add to the LWJGL key buffer
           (.set impl-field nil impl)
-          (when-not @shortcuts/down?
-            (.put key-buf (awt->lwjgl (.getKeyCode e)) (byte 1)))
+          (.put key-buf (awt->lwjgl (.getKeyCode e)) (byte 1))
+          ; pass the event to LibGDX
           (-> game
               .getInput
               .getInputProcessor
