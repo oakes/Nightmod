ff6f34c95121a5c9a025841d4b863d2864e5b3db
oakes <zsoakes@gmail.com>
2014-06-04 14:16:14 -0400

    Stop timers when changing screens

--- src/clojure/nightmod/game.clj
+++ src/clojure/nightmod/game.clj
@@ -4,7 +4,8 @@
             [nightmod.manager :as manager]
             [nightmod.screens :as screens]
             [nightmod.utils :as u]
-            [play-clj.core :refer :all]))
+            [play-clj.core :refer :all]
+            [play-clj.utils :refer :all]))
 
 (defn set-game-screen!
   "Displays one or more screens that you defined with `defscreen`. They will be
@@ -14,9 +15,10 @@ whatever was drawn by the preceding screens.
 
     (set-game-screen! main-screen text-screen)"
   [& game-screens]
-  (on-gl ; clear existing assets so they are reloaded
+  (on-gl ; clear existing timers and assets
+         (stop-timers!)
          (doseq [gs game-screens]
-           (-> gs :screen (swap! #(dissoc % :renderer :layers))))
+           (-> gs :screen (swap! #(dissoc % :timers :renderer :layers))))
          (asset-manager! manager/manager :clear)
          ; set the supplied screen(s) with the overlay screen added at the end
          (apply set-screen!
