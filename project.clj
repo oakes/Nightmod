32b5684242f0a9c271dad99e2733d8a414afc196
oakes <zsoakes@gmail.com>
2014-08-02 15:41:46 -0400

    Use play-clj 0.3.9-SNAPSHOT

--- project.clj
+++ project.clj
@@ -24,7 +24,7 @@
                                play-clj/lein-template]]
                  [org.clojars.oakes/clojail "1.0.6"]
                  [org.clojure/clojure "1.6.0"]
-                 [play-clj "0.3.8"]
+                 [play-clj "0.3.9-SNAPSHOT"]
                  [seesaw "1.4.4"]]
   :uberjar-exclusions [#"clojure-clr.*\.zip"]
   :resource-paths ["resources"]
