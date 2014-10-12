9a941470125ada6b4c5228e3350a59449a1b22be
oakes <zsoakes@gmail.com>
2014-10-12 18:34:53 -0400

    Use play-clj 0.4.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -27,7 +27,7 @@
                  [org.clojure/clojure "1.6.0"]
                  [org.clojure/core.logic "0.8.8"]
                  [org.clojure/tools.reader "0.8.8"]
-                 [play-clj "0.4.0"]
+                 [play-clj "0.4.1-SNAPSHOT"]
                  [play-clj.net "0.1.2"]
                  [seesaw "1.4.4"]]
   :resource-paths ["resources"]
