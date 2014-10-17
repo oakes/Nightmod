e2364d353e2d3c175577f65bdb173f580445ce24
oakes <zsoakes@gmail.com>
2014-10-17 14:34:40 -0400

    Update play-clj to 0.4.1

--- project.clj
+++ project.clj
@@ -27,7 +27,7 @@
                  [org.clojure/clojure "1.6.0"]
                  [org.clojure/core.logic "0.8.8"]
                  [org.clojure/tools.reader "0.8.9"]
-                 [play-clj "0.4.1-SNAPSHOT"]
+                 [play-clj "0.4.1"]
                  [play-clj.net "0.1.2"]
                  [seesaw "1.4.4"]]
   :resource-paths ["resources"]
