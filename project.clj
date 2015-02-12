5c03e8bad38f72c1f97c5b4e599736328415bbe9
oakes <zsoakes@gmail.com>
2015-02-11 19:46:21 -0500

    Update libraries

--- project.clj
+++ project.clj
@@ -4,15 +4,15 @@
   :license {:name "Public Domain"
             :url "http://unlicense.org/UNLICENSE"}
   :dependencies [[clojail "1.0.6"]
-                 [com.badlogicgames.gdx/gdx "1.5.0"]
-                 [com.badlogicgames.gdx/gdx-backend-lwjgl "1.5.0"]
-                 [com.badlogicgames.gdx/gdx-box2d "1.5.0"]
-                 [com.badlogicgames.gdx/gdx-box2d-platform "1.5.0"
+                 [com.badlogicgames.gdx/gdx "1.5.3"]
+                 [com.badlogicgames.gdx/gdx-backend-lwjgl "1.5.3"]
+                 [com.badlogicgames.gdx/gdx-box2d "1.5.3"]
+                 [com.badlogicgames.gdx/gdx-box2d-platform "1.5.3"
                   :classifier "natives-desktop"]
-                 [com.badlogicgames.gdx/gdx-bullet "1.5.0"]
-                 [com.badlogicgames.gdx/gdx-bullet-platform "1.5.0"
+                 [com.badlogicgames.gdx/gdx-bullet "1.5.3"]
+                 [com.badlogicgames.gdx/gdx-bullet-platform "1.5.3"
                   :classifier "natives-desktop"]
-                 [com.badlogicgames.gdx/gdx-platform "1.5.0"
+                 [com.badlogicgames.gdx/gdx-platform "1.5.3"
                   :classifier "natives-desktop"]
                  [nightcode "0.4.3"
                   :exclusions [gwt-plugin
@@ -27,10 +27,10 @@
                  [org.clojure/clojure "1.6.0"]
                  [org.clojure/core.logic "0.8.9"]
                  [org.clojure/tools.reader "0.8.13"]
-                 [play-clj "0.4.3"]
+                 [play-clj "0.4.4-SNAPSHOT"]
                  [play-clj.net "0.1.2"]
-                 [prismatic/schema "0.3.3"]
-                 [seesaw "1.4.4"]]
+                 [prismatic/schema "0.3.7"]
+                 [seesaw "1.4.5"]]
   :resource-paths ["resources"]
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java"]
