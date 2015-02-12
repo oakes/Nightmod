06b2010e302d9f95e5020589545b70cb23054906
oakes <zsoakes@gmail.com>
2015-02-12 18:49:40 -0500

    Start 0.2.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nightmod "0.2.4"
+(defproject nightmod "0.2.5-SNAPSHOT"
   :description "A tool for making live-moddable games in Clojure"
   :url "https://github.com/oakes/Nightmod"
   :license {:name "Public Domain"
