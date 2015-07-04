b440ca1750d940d948d2a665b1c267804c3c5786
oakes <zsoakes@gmail.com>
2015-07-04 12:41:43 -0400

    Update play-clj to 0.4.7

--- project.clj
+++ project.clj
@@ -4,17 +4,17 @@
   :license {:name "Public Domain"
             :url "http://unlicense.org/UNLICENSE"}
   :dependencies [[clojail "1.0.6"]
-                 [com.badlogicgames.gdx/gdx "1.5.5"]
-                 [com.badlogicgames.gdx/gdx-backend-lwjgl "1.5.5"]
-                 [com.badlogicgames.gdx/gdx-box2d "1.5.5"]
-                 [com.badlogicgames.gdx/gdx-box2d-platform "1.5.5"
+                 [com.badlogicgames.gdx/gdx "1.6.0"]
+                 [com.badlogicgames.gdx/gdx-backend-lwjgl "1.6.0"]
+                 [com.badlogicgames.gdx/gdx-box2d "1.6.0"]
+                 [com.badlogicgames.gdx/gdx-box2d-platform "1.6.0"
                   :classifier "natives-desktop"]
-                 [com.badlogicgames.gdx/gdx-bullet "1.5.5"]
-                 [com.badlogicgames.gdx/gdx-bullet-platform "1.5.5"
+                 [com.badlogicgames.gdx/gdx-bullet "1.6.0"]
+                 [com.badlogicgames.gdx/gdx-bullet-platform "1.6.0"
                   :classifier "natives-desktop"]
-                 [com.badlogicgames.gdx/gdx-platform "1.5.5"
+                 [com.badlogicgames.gdx/gdx-platform "1.6.0"
                   :classifier "natives-desktop"]
-                 [nightcode "0.4.6"
+                 [nightcode "0.4.7"
                   :exclusions [gwt-plugin
                                leiningen
                                lein-ancient
@@ -24,12 +24,12 @@
                                lein-fruit
                                lein-typed
                                play-clj/lein-template]]
-                 [org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojure "1.7.0"]
                  [org.clojure/core.logic "0.8.10"]
-                 [org.clojure/tools.reader "0.8.16"]
-                 [play-clj "0.4.6"]
+                 [org.clojure/tools.reader "0.9.2"]
+                 [play-clj "0.4.7"]
                  [play-clj.net "0.1.2"]
-                 [prismatic/schema "0.4.0"]
+                 [prismatic/schema "0.4.3"]
                  [seesaw "1.4.5"]]
   :resource-paths ["resources"]
   :source-paths ["src/clojure"]
