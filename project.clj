ddff123d777f4852be019db4018acc0b0b495fe8
oakes <zsoakes@gmail.com>
2015-07-04 13:27:47 -0400

    Increment version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nightmod "0.2.7-SNAPSHOT"
+(defproject nightmod "0.2.7"
   :description "A tool for making live-moddable games in Clojure"
   :url "https://github.com/oakes/Nightmod"
   :license {:name "Public Domain"
