6032efc02494cb90986f8c3d5062d2bcfc8097cb
oakes <zsoakes@gmail.com>
2016-01-03 00:45:58 -0500

    Update play-clj and libGDX

--- project.clj
+++ project.clj
@@ -1,33 +1,32 @@
-(defproject nightmod "0.2.8-SNAPSHOT"
+(defproject nightmod "1.0.0-SNAPSHOT"
   :description "A tool for making live-moddable games in Clojure"
   :url "https://github.com/oakes/Nightmod"
   :license {:name "Public Domain"
             :url "http://unlicense.org/UNLICENSE"}
   :dependencies [[clojail "1.0.6"]
-                 [com.badlogicgames.gdx/gdx "1.6.0"]
-                 [com.badlogicgames.gdx/gdx-backend-lwjgl "1.6.0"]
-                 [com.badlogicgames.gdx/gdx-box2d "1.6.0"]
-                 [com.badlogicgames.gdx/gdx-box2d-platform "1.6.0"
+                 [com.badlogicgames.gdx/gdx "1.7.2"]
+                 [com.badlogicgames.gdx/gdx-backend-lwjgl "1.7.2"]
+                 [com.badlogicgames.gdx/gdx-box2d "1.7.2"]
+                 [com.badlogicgames.gdx/gdx-box2d-platform "1.7.2"
                   :classifier "natives-desktop"]
-                 [com.badlogicgames.gdx/gdx-bullet "1.6.0"]
-                 [com.badlogicgames.gdx/gdx-bullet-platform "1.6.0"
+                 [com.badlogicgames.gdx/gdx-bullet "1.7.2"]
+                 [com.badlogicgames.gdx/gdx-bullet-platform "1.7.2"
                   :classifier "natives-desktop"]
-                 [com.badlogicgames.gdx/gdx-platform "1.6.0"
+                 [com.badlogicgames.gdx/gdx-platform "1.7.2"
                   :classifier "natives-desktop"]
-                 [nightcode "0.4.7"
+                 [nightcode "1.0.0-SNAPSHOT"
                   :exclusions [gwt-plugin
                                leiningen
                                lein-ancient
                                lein-cljsbuild
                                lein-clr
                                lein-droid
-                               lein-fruit
                                lein-typed
                                play-clj/lein-template]]
                  [org.clojure/clojure "1.7.0"]
                  [org.clojure/core.logic "0.8.10"]
                  [org.clojure/tools.reader "0.9.2"]
-                 [play-clj "0.4.7"]
+                 [play-clj "1.0.0-SNAPSHOT"]
                  [play-clj.net "0.1.2"]
                  [prismatic/schema "0.4.3"]
                  [seesaw "1.4.5"]]
