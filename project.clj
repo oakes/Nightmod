ea0b65e01190e41fe2af90c9c6eaed8f8530bbd4
oakes <zsoakes@gmail.com>
2016-04-07 10:39:10 -0400

    Update nightcode to 1.2.0

--- project.clj
+++ project.clj
@@ -14,7 +14,7 @@
                   :classifier "natives-desktop"]
                  [com.badlogicgames.gdx/gdx-platform "1.8.0"
                   :classifier "natives-desktop"]
-                 [nightcode "1.1.3"
+                 [nightcode "1.2.0"
                   :exclusions [gwt-plugin
                                leiningen
                                lein-ancient
